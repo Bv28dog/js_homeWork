9639f8475fa4dd70ffc0ef94ef93e3f9b0500a2a
Ostap <bv28dog@gmail.com>
2023-10-13 21:16:35 +0300

    complete task

--- dom/index.js
+++ dom/index.js
@@ -1,46 +1,63 @@
-class ProductsService {
-  static showSuccessMessage(message) {
-    const notification = document.getElementById("notification");
-    const notificationText = document.getElementById("notificationText");
-
-    notification.className = "notification success";
-    notificationText.innerText = message;
-    notification.style.display = "block";
-    notificationText.style.backgroundColor = "green";
-    notificationText.style.textAlign = "center";
-    notificationText.style.display = "flex";
-    notificationText.style.justifyContent = "center";
-    notificationText.style.borderRadius = "20px";
-    notificationText.style.alignItems = " center";
-    notificationText.style.height = "100px";
-    notificationText.style.color = "white";
-    notificationText.style.fontSize = "40px";
-
-    setTimeout(() => {
-      notification.style.display = "none";
-    }, 10000);
+class NotificationService {
+  constructor() {
+    this.notification = document.getElementById("notification");
+    this.notificationText = document.getElementById("notificationText");
   }
 
-  static showErrorMessage(errorMessage) {
-    const notification = document.getElementById("notification");
-    const notificationText = document.getElementById("notificationText");
-
-    notification.className = "notification error";
-    notificationText.innerText = errorMessage;
-    notification.style.display = "block";
-    notificationText.style.backgroundColor = "red";
-    notificationText.style.textAlign = "center";
-    notificationText.style.display = "flex";
-    notificationText.style.justifyContent = "center";
-    notificationText.style.borderRadius = "20px";
-    notificationText.style.height = "100px";
-    notificationText.style.color = "black";
-    notificationText.style.fontSize = "40px";
+  showMessage(message, className, duration) {
+    this.notification.className = "notification " + className;
+    this.notificationText.innerText = message;
+    this.notification.style.display = "block";
+    if (className === "success") {
+      this.notificationText.style.backgroundColor = "green";
+      this.notificationText.style.textAlign = "center";
+      this.notificationText.style.display = "flex";
+      this.notificationText.style.justifyContent = "center";
+      this.notificationText.style.borderRadius = "20px";
+      this.notificationText.style.alignItems = " center";
+      this.notificationText.style.height = "100px";
+      this.notificationText.style.color = "white";
+      this.notificationText.style.fontSize = "40px";
+    } else {
+      this.notificationText.style.backgroundColor = "red";
+      this.notificationText.style.textAlign = "center";
+      this.notificationText.style.display = "flex";
+      this.notificationText.style.justifyContent = "center";
+      this.notificationText.style.borderRadius = "20px";
+      this.notificationText.style.alignItems = " center";
+      this.notificationText.style.height = "100px";
+      this.notificationText.style.color = "black";
+      this.notificationText.style.fontSize = "40px";
+    }
 
     setTimeout(() => {
-      notification.style.display = "none";
-    }, 1000);
+      this.notification.style.display = "none";
+    }, duration);
   }
 }
-console.log(ProductsService.showErrorMessage("Error"));
-console.log(ProductsService.showSuccessMessage("Success"));
+
+class SuccessNotification extends NotificationService {
+  constructor() {
+    super();
+  }
+
+  showSuccessMessage(message, duration = 10000) {
+    this.showMessage(message, "success", duration);
+  }
+}
+
+class ErrorNotification extends NotificationService {
+  constructor() {
+    super();
+  }
+
+  showErrorMessage(message, duration = 1000) {
+    this.showMessage(message, "error", duration);
+  }
+}
+
+const successNotificationService = new SuccessNotification();
+const errorNotificationService = new ErrorNotification();
+
+console.log(successNotificationService.showSuccessMessage("Success"));
+console.log(errorNotificationService.showErrorMessage("Error"));
